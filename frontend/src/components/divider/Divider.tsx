33d8766a98895111d931a952ac7c283e0a801c14
Maidana <franco_maidana07@hotmail.com>
2024-11-14 21:58:27 -0300

    footer resize

--- frontend/src/components/divider/Divider.tsx
+++ frontend/src/components/divider/Divider.tsx
@@ -1,7 +1,7 @@
 
 const Divider = () => {
   return (
-    <div className="border border-[#8d8d8d] w-full h-[1px]"></div>
+    <div className="border border-[#8d8d8d] w-full h-[1px] my-3"></div>
   )
 }
 
