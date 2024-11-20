b2d339217cb70229bb2935b1869222290fb4c8ab
illiCristian <svrk73@gmail.com>
2024-11-20 19:43:14 -0300

    Feat: added swaager documentation on admin dashboard controller

--- backend/src/admin/admin-users/admin-users.controller.ts
+++ backend/src/admin/admin-users/admin-users.controller.ts
@@ -1,35 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { AdminUsersService } from './admin-users.service';
 import { CreateAdminUserDto } from './dto/create-admin-user.dto';
 import { UpdateAdminUserDto } from './dto/update-admin-user.dto';
-
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+@UseGuards(AuthGuard, RolesGuard)
+@Roles('superAdmin', 'admin')
 @Controller('admin-users')
 export class AdminUsersController {
   constructor(private readonly adminUsersService: AdminUsersService) {}
 
-  @Get()
-  findAll() {
-    return this.adminUsersService.findAll();
-  }
 
   @Post('deactivate-inactive-users')
   deactivateInactiveUsers(@Query('months') months: string) {
-    const monthsNumber = parseInt(months, 10) || 3; // Por defecto, 3 meses
+    const monthsNumber = parseInt(months, 10) || 3;
     return this.adminUsersService.deactivateInactiveUsers(monthsNumber);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.adminUsersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdminUserDto: UpdateAdminUserDto) {
-    return this.adminUsersService.update(+id, updateAdminUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.adminUsersService.remove(+id);
-  }
 }
--- backend/src/admin/admin-users/admin-users.service.ts
+++ backend/src/admin/admin-users/admin-users.service.ts
@@ -23,19 +23,5 @@ export class AdminUsersService {
     };
   }
 
-  findAll() {
-    return `This action returns all adminUsers`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} adminUser`;
-  }
-
-  update(id: number, updateAdminUserDto: UpdateAdminUserDto) {
-    return `This action updates a #${id} adminUser`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} adminUser`;
-  }
+ 
 }
