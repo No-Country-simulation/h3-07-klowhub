7070d8e06ff26691efcd52ca5b40b9f96bd55718
Maidana <franco_maidana07@hotmail.com>
2024-11-21 18:48:00 -0300

    home (for now)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,42 +5,41 @@ import CardSection from "@/components/common/CardSection";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-5 my-10" >
+    <div className="flex flex-col items-center justify-center gap-5 my-10" >
 
-        <CardSection
-          title="Continuá tu aprendizaje"
-          description="Retomá donde lo dejaste. Volvé a ver tu último video y seguí aprendiendo sin perder el ritmo."
-          button={false}
-        >
-          <ProgressCard />
-        </CardSection>
-        <CardSection
-          title="Cursos recomendados"
-          description="Descubre los cursos más destacados y lleva tus habilidades al siguiente nivel. Aprende de expertos y aplica tus conocimientos en proyectos reales."
-        >
-          <CardShop isFor="course" />
-          <CardShop isFor="course" platform="Power Apps" />
-        </CardSection>
+      <CardSection
+        title="Continuá tu aprendizaje"
+        description="Retomá donde lo dejaste. Volvé a ver tu último video y seguí aprendiendo sin perder el ritmo."
+        button={false}
+      >
+        <ProgressCard />
+      </CardSection>
 
-        <CardSection
-          title="Aplicaciones recomendadas"
-          description="Explorá soluciones listas para usar. Encontrá la app perfecta para tu proyecto y empezá a trabajar de inmediato."
-        >
-          <CardShop isFor="app" />
-          <CardShop isFor="app" platform="Power Apps" />
-          <CardShop isFor="app" />
-        </CardSection>
+      <CardSection
+        title="Cursos recomendados"
+        description="Descubre los cursos más destacados y lleva tus habilidades al siguiente nivel. Aprende de expertos y aplica tus conocimientos en proyectos reales."
+      >
+        <CardShop isFor="course" />
+        <CardShop isFor="course" platform="Power Apps" />
+      </CardSection>
 
-        <CardSection
-          button={false}
-        >
-          <CardShopConnect />
-          <CardShopConnect platform="Power Apps" />
-          <CardShopConnect />
-        </CardSection>
+      <CardSection
+        title="Aplicaciones recomendadas"
+        description="Explorá soluciones listas para usar. Encontrá la app perfecta para tu proyecto y empezá a trabajar de inmediato."
+      >
+        <CardShop isFor="app" />
+        <CardShop isFor="app" platform="Power Apps" />
+        <CardShop isFor="app" />
+      </CardSection>
 
-      </div >
-    </>
+      <CardSection
+        button={false}
+      >
+        <CardShopConnect />
+        <CardShopConnect platform="Power Apps" />
+        <CardShopConnect />
+      </CardSection>
+
+    </div >
   );
 }
